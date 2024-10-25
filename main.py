a7532cdf9143a5cf630681719c588015253e9568
NickBwalley <nickbwalley@gmail.com>
2024-10-25 22:55:09 +0200

    python crashcourse

---
+++ main.py
@@ -0,0 +1,24 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+# CrashCourse
+# 1-- variables.
+age = 20 #integer
+price = 19.95 #float
+first_name = "Nick" #string
+is_online = True #boolean
+print(age)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
